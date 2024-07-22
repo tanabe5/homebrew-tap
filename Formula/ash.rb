eb83130a999f45ceec88241fea31612224058bc0
tanabe5 <166473160+tanabe5@users.noreply.github.com>
2024-07-22 21:04:37 +0900

    Update ash.rb

--- Formula/ash.rb
+++ Formula/ash.rb
@@ -12,22 +12,22 @@ class Ash < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tanabe5/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_amd64.tar.gz"
-    sha256 "1650125eb012e89a9f8fd0369feb8386d0c831880af912673dd00bc504976e6f"
+    sha256 "e9752c5965da9385727156d815337d7db7ce3cdaa8de03772f3b6fab566fcb7a"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/tanabe5/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_arm64.tar.gz"
-    sha256 "92ea58c3c873ecc8ea52c39b48f30af10e538169725504d6353fa43cf48984cd"
+    sha256 "22b4f4a443a80534494592b96cb4eec9a3e25caf296197f3cec534bfc464b5f3"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/tanabe5/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_amd64.tar.gz"
-    sha256 "14fa2c2bdc61eee09abf03a1caea0f6d17a0918b6dc78f6b9dd1d24180236a3f"
+    sha256 "f19716ad82fa8dd2079ee9f4f76935e8e16e475014598427a8668c69c4fb8313"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/tanabe5/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_arm64.tar.gz"
-    sha256 "509395e8ac029a8c5b0ae76057b9c1c3a94b294cb1a808875ef3426dc965d09a"
+    sha256 "d6bc789afdae200dee00e6782d8c46b0c01753ee838a9deaea636905cbdcdd21"
   end
 
   def install
